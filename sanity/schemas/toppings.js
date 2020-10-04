565efbbb2682c5a8cb357f89a33a996004928eb8
Clint Milner <cmilner@datto.com>
2020-10-04 20:37:16 +0100

    sanity pizza and toppings schema

---
+++ sanity/schemas/toppings.js
@@ -0,0 +1,34 @@
+import { FaPepperHot as icon } from "react-icons/fa";
+
+export default {
+  name: "topping",
+  title: "Toppings",
+  type: "document",
+  icon,
+  fields: [
+    {
+      name: "name",
+      title: "Topping Name",
+      type: "string",
+      description: "Name of the topping",
+    },
+    {
+      name: "vegetarian",
+      title: "Vegetarian",
+      type: "boolean",
+      description: "Is this a vegetarian pizza?",
+      options: {
+        layout: "switch",
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: "name",
+      vegetarian: "vegetarian",
+    },
+    prepare: (fields) => ({
+      title: `${fields?.name} ${fields?.vegetarian ? "🥬" : ""}`,
+    }),
+  },
+};
